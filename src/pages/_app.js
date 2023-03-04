a625db85a15e3b789a9493292dab628f7f544ba0
Alex Lau <alex.wanna.nirvana@gmail.com>
2023-03-04 16:04:34 +0800

    chore: set ESlint and Prettier

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
+import { Global, css } from '@emotion/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          body {
+            font-family: Arial, Helvetica, sans-serif;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
